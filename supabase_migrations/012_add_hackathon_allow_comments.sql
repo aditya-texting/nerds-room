4222ab7d4b985a7a473353f8a711a27b7cee5e42
Sahil Sumrani <147994652+Sahilarora83@users.noreply.github.com>
2026-02-17 22:53:09 +0530

    Delete supabase_migrations directory

--- supabase_migrations/012_add_hackathon_allow_comments.sql
+++
@@ -1,5 +0,0 @@
--- Migration 012: Add allow_comments to hackathons table
--- This allows setting a default comment permission at the hackathon level
-
-ALTER TABLE public.hackathons 
-ADD COLUMN IF NOT EXISTS allow_comments BOOLEAN DEFAULT true;
